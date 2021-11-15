d33eee7d5df3cecc55c204ce4b3f59df25f6b969
Lucas Bohórquez <lucas.1701716343@ucaldas.edu.co>
2021-11-15 17:59:01 -0500

    Adding authentication

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\PostsController;
-use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 use PHPUnit\TextUI\XmlConfiguration\Group;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,6 @@ Route::prefix('dashboard')->group(function () {
     Route::resource('posts', PostsController::class);
     Route::resource('category', CategoryController::class);
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
